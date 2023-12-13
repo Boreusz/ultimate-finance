0e52dabd6a7fdcf7caa3c76b53c926fddfda74d4
Mateusz <matt.borowczyk@gmail.com>
2023-12-13 22:56:02 +0800

    add essential dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,16 @@ export default defineConfig({
   server: {
     watch: {
       usePolling: true,
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@assets": path.resolve(__dirname, "./src/assets"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@router": path.resolve(__dirname, "./src/router"),
+      "@views": path.resolve(__dirname, "./src/views"),
+      "@interfaces": path.resolve(__dirname, "./src/interfaces"),
     },
   },
 });
