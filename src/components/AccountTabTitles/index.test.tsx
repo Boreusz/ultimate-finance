514134391b1c36a43b81c22ba43aa1d68c72aef3
Mateusz <matt.borowczyk@gmail.com>
2023-12-13 22:56:02 +0800

    improve precision of the AccountTabTittles test cases

--- src/components/AccountTabTitles/index.test.tsx
+++ src/components/AccountTabTitles/index.test.tsx
@@ -3,7 +3,7 @@ import { expect, test } from "vitest";
 import AccountTabTitles from ".";
 import { Table } from "@chakra-ui/react";
 
-test("GIVEN AccountTabTitles WHEN there are accounts to display THEN Table gets populated", () => {
+test("GIVEN AccountTabTitles WHEN position is set top 'top' THEN Table Header is displayed", () => {
   render(
     <Table>
       <AccountTabTitles position="top" />
@@ -14,9 +14,22 @@ test("GIVEN AccountTabTitles WHEN there are accounts to display THEN Table gets 
     screen.queryByLabelText("AccountTable-Footer")
   ).not.toBeInTheDocument();
   expect(screen.queryByLabelText("AccountTable-Header")).toBeInTheDocument();
+
+  expect(screen.queryByLabelText("Column-AccountName")).toHaveTextContent(
+    "Account Name"
+  );
+  expect(screen.queryByLabelText("Column-AccountType")).toHaveTextContent(
+    "Account Type"
+  );
+  expect(screen.queryByLabelText("Column-Balance")).toHaveTextContent(
+    "Balance"
+  );
+  expect(screen.queryByLabelText("Column-Actions")).toHaveTextContent(
+    "Actions"
+  );
 });
 
-test("GIVEN AccountTabTitles WHEN there are accounts to display THEN Table gets populated", () => {
+test("GIVEN AccountTabTitles WHEN position is set top 'bottom' THEN Table Footer is displayed", () => {
   render(
     <Table>
       <AccountTabTitles position="bottom" />
@@ -27,4 +40,17 @@ test("GIVEN AccountTabTitles WHEN there are accounts to display THEN Table gets 
   expect(
     screen.queryByLabelText("AccountTable-Header")
   ).not.toBeInTheDocument();
+
+  expect(screen.queryByLabelText("Column-AccountName")).toHaveTextContent(
+    "Account Name"
+  );
+  expect(screen.queryByLabelText("Column-AccountType")).toHaveTextContent(
+    "Account Type"
+  );
+  expect(screen.queryByLabelText("Column-Balance")).toHaveTextContent(
+    "Balance"
+  );
+  expect(screen.queryByLabelText("Column-Actions")).toHaveTextContent(
+    "Actions"
+  );
 });
